c6c7e8875e383ada0a9be89b707026139af00db1
Robert Durcansky <robert.durcansky@embedit.cz>
2022-03-03 16:23:26 +0100

    Add eslint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewSiteComponent } from './new-site/new-site.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'new-site',
+    component: NewSiteComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewSiteComponent } from './new-site/new-site.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewSiteComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
